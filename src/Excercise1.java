3eb57975d9bd2119e3b004624e8022d7d7c3e979
unknown <TurmDrummer@Peter.(none)>
2012-03-07 18:38:03 +0100

    Static Main Write to Console

---
+++ src/Excercise1.java
@@ -0,0 +1,13 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: TurmDrummer
+ * Date: 07.03.12
+ * Time: 18:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class Excercise1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Consolen Ausgabe");
+    }
+}
